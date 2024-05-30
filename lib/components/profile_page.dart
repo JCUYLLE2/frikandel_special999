fed2fc94c961d1ff748877b2470efde663a986ce
JCUYLLE2 <162974022+JCUYLLE2@users.noreply.github.com>
2024-05-30 11:02:33 +0200

    profile pic OK

--- lib/components/profile_page.dart
+++ lib/components/profile_page.dart
@@ -1,6 +1,9 @@
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProfilePage extends StatefulWidget {
   final VoidCallback callback;
@@ -13,8 +16,10 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   String username = 'Loading...';
+  String? profileImageUrl;
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final ImagePicker _picker = ImagePicker();
 
   @override
   void initState() {
@@ -33,6 +38,9 @@ class _ProfilePageState extends State<ProfilePage> {
           var data = userDoc.data() as Map<String, dynamic>;
           setState(() {
             username = data['username'] ?? 'No Username';
+            profileImageUrl = data.containsKey('profileImageUrl')
+                ? data['profileImageUrl']
+                : null;
           });
         } else {
           print('Gebruikersdocument niet gevonden');
@@ -54,6 +62,34 @@ class _ProfilePageState extends State<ProfilePage> {
     }
   }
 
+  Future<void> _uploadProfileImage() async {
+    final XFile? imageFile =
+        await _picker.pickImage(source: ImageSource.gallery);
+    if (imageFile != null) {
+      Uint8List imageData = await imageFile.readAsBytes();
+
+      User? user = _auth.currentUser;
+      if (user != null) {
+        final Reference storageRef = FirebaseStorage.instance
+            .ref()
+            .child('profile_images')
+            .child('${user.uid}.jpg');
+        final UploadTask uploadTask = storageRef.putData(imageData);
+        final TaskSnapshot taskSnapshot =
+            await uploadTask.whenComplete(() => {});
+        final String downloadUrl = await taskSnapshot.ref.getDownloadURL();
+
+        await _firestore.collection('users').doc(user.uid).set({
+          'profileImageUrl': downloadUrl,
+        }, SetOptions(merge: true));
+
+        setState(() {
+          profileImageUrl = downloadUrl;
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,9 +100,25 @@ class _ProfilePageState extends State<ProfilePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            if (profileImageUrl != null)
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(profileImageUrl!),
+              )
+            else
+              CircleAvatar(
+                radius: 50,
+                child: Icon(Icons.person, size: 50),
+              ),
+            const SizedBox(height: 20),
             Text(
               'Welcome, $username!',
               style: const TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _uploadProfileImage,
+              child: const Text('Upload Profielfoto'),
             ),
             const SizedBox(height: 20),
             ElevatedButton(
